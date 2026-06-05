663b5a22a2335d667be7a49dd6e1052f0da21b9a
csotoRBK <csoto@rubrika.cl>
2026-06-05 14:46:22 -0400

    nuevo loader

--- resources/views/components/page-loader.blade.php
+++ resources/views/components/page-loader.blade.php
@@ -1,14 +1,12 @@
 <div id="page-loader" aria-hidden="true" aria-live="polite" role="status">
-    <div class="page-loader__panel">
+    <div class="page-loader__scene">
+        <span class="page-loader__track" aria-hidden="true"></span>
         <img
-            src="{{ asset('images/loading.gif') }}"
-            data-fallback="{{ asset('images/loading.svg') }}"
-            class="page-loader__media"
+            src="{{ asset('images/cart-loader.svg') }}"
+            class="page-loader__cart"
             alt=""
-            width="72"
-            height="72"
-            onerror="if (!this.dataset.fallbackUsed) { this.dataset.fallbackUsed='1'; this.src=this.dataset.fallback; }"
+            width="88"
+            height="56"
         >
-        <p class="page-loader__text">Cargando...</p>
     </div>
 </div>
