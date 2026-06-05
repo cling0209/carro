c9b21c9fba2041453e535c9679b4332ab70d283d
csotoRBK <csoto@rubrika.cl>
2026-06-05 00:05:12 -0400

    cambio config productos

--- config/products.php
+++ config/products.php
@@ -11,13 +11,13 @@ return [
     | filename = str_replace('{codigo}', sku, image_filename_pattern)
     |
     | Ejemplo:
-    | https://www.romulo.cl/allproducts/imagenes/productos/electronica/AUD-001_medium.jpg
+    | https://www.romulo.cl/allproducts/imagenes/productos/electronica/AUD-001.jpg
     |
     */
 
     'image_base_url' => env('PRODUCT_IMAGE_BASE_URL'),
 
-    'image_filename_pattern' => env('PRODUCT_IMAGE_FILENAME_PATTERN', '{codigo}_medium.jpg'),
+    'image_filename_pattern' => env('PRODUCT_IMAGE_FILENAME_PATTERN', '{codigo}.jpg'),
 
     'image_fallback_url' => env(
         'PRODUCT_IMAGE_FALLBACK_URL',
